3367cf9f7c64d170b444fd5e6249705c608e9c9b
AjayShukla007 <shuklaajay0838@gmail.com>
2024-08-22 11:15:29 +0530

    improve code structure

--- routes/caseStudies.js
+++ routes/caseStudies.js
@@ -7,7 +7,16 @@ const fetchData = require("../middleware/getUser");
 
 router.post("/postCs", fetchData, async (req, res) => {
   try {
-    const { title, id, description, flow, feature, techUsed, motivation, conclusion } = req.body;
+    const {
+      title,
+      id,
+      description,
+      flow,
+      feature,
+      techUsed,
+      motivation,
+      conclusion,
+    } = req.body;
     const newCaseStudy = new CaseStudies({
       title,
       id,
@@ -17,13 +26,13 @@ router.post("/postCs", fetchData, async (req, res) => {
       techUsed,
       motivation,
       conclusion,
-      user: req.user.id
+      user: req.user.id,
     });
     const saveCaseStudy = await newCaseStudy.save();
   } catch (e) {
     res.status(500).json({
       message: "some error occured",
-      error: e
+      error: e,
     });
     console.log(e);
   }
@@ -37,67 +46,71 @@ router.get("/getCs/:title", fetchData, async (req, res) => {
   } catch (e) {
     res.status(500).json({
       message: "could not send case studies data right now",
-      error: e
+      error: e,
     });
   }
 });
 
 router.get("/getCsTitle", fetchData, async (req, res) => {
   try {
-    const titles = await CaseStudies.distinct('title');
+    const titles = await CaseStudies.distinct("title");
     if (!titles) {
       res.status(404).json({
-        message:"cannot find any titles did you created any case studies"
+        message: "cannot find any titles did you created any case studies",
       });
     }
     res.json(titles);
   } catch (e) {
     res.status(500).json({
       message: "could not send case studies titles data right now",
-      error: e
+      error: e,
     });
   }
 });
 
-router.patch("/updateCs/:id", fetchData, async (req, res)=>{
+router.patch("/updateCs/:id", fetchData, async (req, res) => {
   try {
     const caseStudyId = req.params.id;
     const updateFields = req.body;
     // const updateCaseStudy = await CaseStudies.findByIdAndUpdate(caseStudyId, updateFields, {new: true});
-    const updateCaseStudy = await CaseStudies.findByIdAndUpdate(caseStudyId, updateFields, { new: true });
+    const updateCaseStudy = await CaseStudies.findByIdAndUpdate(
+      caseStudyId,
+      updateFields,
+      { new: true }
+    );
 
     if (!updateCaseStudy) {
       return res.status(404).json({
-        message: "case study not found or some error occured"
+        message: "case study not found or some error occured",
       });
     }
     res.status(200).json({
-      mesaage: "case study updated successfully"
+      mesaage: "case study updated successfully",
     });
   } catch (e) {
     res.json({
       message: "unable to update",
-      error: e
+      error: e,
     });
     console.log(e);
   }
 });
 
-router.delete("/deleteCs/:id",fetchData, async (req, res)=>{
+router.delete("/deleteCs/:id", fetchData, async (req, res) => {
   try {
     const caseStudyId = req.params.id;
     const deleteCs = await CaseStudies.findByIdAndRemove(caseStudyId);
-   if (!deleteCs) {
+    if (!deleteCs) {
       return res.status(404).json({ message: "Case study not found" });
     }
     res.status(200).json({
       message: "Case study deleted successfully",
-      deleteCs
+      deleteCs,
     });
   } catch (e) {
     res.json({
-      message:"unable to delete vase study",
-      error: e
+      message: "unable to delete vase study",
+      error: e,
     });
     console.log(e);
   }
