6d8532bb3e3dd155cf05f59030715764933b2cb1
AjayShukla007 <shuklaajay0838@gmail.com>
2024-09-09 11:15:29 +0530

    improve code structure

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = async() => {
+const connectToMongo = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
